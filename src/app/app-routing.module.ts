e14665fe9c0dc8b4f5f544b0de068d614b93cf42
sanketmorkhade <morkhade.sanket7@gmail.com>
2020-01-05 12:03:59 +0530

    4 files modified, 2 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { HomePageComponent } from './home-page/home-page.component';
+import { LoginComponent } from './login/login.component';
 import { JobListingComponent } from './job-listing/job-listing.component';
 import { CreateJobComponent } from './create-job/create-job.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home'},
   { path: 'home', component: HomePageComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'jobs', component: JobListingComponent },
   { path: 'createJob', component: CreateJobComponent },
 
@@ -18,7 +20,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes), CommonModule, ReactiveFormsModule ],
-  declarations: [ HomePageComponent, JobListingComponent, CreateJobComponent ],
+  declarations: [ HomePageComponent, LoginComponent, JobListingComponent, CreateJobComponent ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
